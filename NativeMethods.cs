2d603d8d60c81635d564d06a2b630a33cf7a454e
Jim Evans <james.h.evans.jr@gmail.com>
2019-04-06 14:52:18 -0700

    Initial checkin of functional code

---
+++ NativeMethods.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace IEProxyTest
+{
+    class NativeMethods
+    {
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct InternetPerConnectionOptionList
+        {
+            public int Size;
+
+            // The connection to be set. NULL means LAN.
+            public IntPtr Connection;
+
+            public int OptionCount;
+            public int OptionError;
+
+            // List of INTERNET_PER_CONN_OPTIONs.
+            public IntPtr OptionsBufferPointer;
+        }
+
+        public enum InternetOption
+        {
+            // Sets or retrieves an INTERNET_PER_CONN_OPTION_LIST structure that specifies
+            // a list of options for a particular connection.
+            PerConnectionOption = 75,
+
+            // Notify the system that the registry settings have been changed so that
+            // it verifies the settings on the next call to InternetConnect.
+            SettingsChanged = 39,
+
+            // Causes the proxy data to be reread from the registry for a handle.
+            Refresh = 37
+
+        }
+
+        public enum InternetPerConnectionOptionValue
+        {
+            Flags = 1,
+            ProxyServer = 2,
+            ProxyBypass = 3,
+            AutoConfigUrl = 4,
+            AutoDiscoveryFlags = 5,
+            AutoConfigSecondaryUrl = 6,
+            AutoConfigReloadDelayMins = 7,
+            AutoConfigLastDetectTime = 8,
+            AutoConfigLastDetectUrl = 9,
+            FlagsUi = 10
+        }
+
+        public const int INTERNET_OPEN_TYPE_PROXY = 3;
+        public const int INTERNET_OPEN_TYPE_DIRECT = 1;  // direct to net
+        public const int INTERNET_OPEN_TYPE_PRECONFIG = 0; // read registry
+
+        /// <summary>
+        /// Constants used in INTERNET_PER_CONN_OPTON struct.
+        /// </summary>
+        public enum InternetPerConnectionProxyFlags
+        {
+            Direct = 0x00000001,   // direct to net
+            Proxy = 0x00000002,   // via named proxy
+            AutoProxyUrl = 0x00000004,   // autoproxy URL
+            AutoDetect = 0x00000008   // use autoproxy detection
+        }
+
+        /// <summary>
+        /// Used in INTERNET_PER_CONN_OPTION.
+        /// When create a instance of OptionUnion, only one filed will be used.
+        /// The StructLayout and FieldOffset attributes could help to decrease the struct size.
+        /// </summary>
+        [StructLayout(LayoutKind.Explicit)]
+        public struct InternetPerConnectionOptionData
+        {
+            // A value in INTERNET_OPTION_PER_CONN_FLAGS.
+            [FieldOffset(0)]
+            public int integerValue;
+            [FieldOffset(0)]
+            public System.IntPtr stringValue;
+            [FieldOffset(0)]
+            public System.Runtime.InteropServices.ComTypes.FILETIME fileTimeValue;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct InternetPerConnectionOption
+        {
+            // A value in INTERNET_PER_CONN_OptionEnum.
+            public int Option;
+            public InternetPerConnectionOptionData Value;
+        }
+
+        /// <summary>
+        /// Sets an Internet option.
+        /// </summary>
+        [DllImport("wininet.dll", CharSet = CharSet.Ansi, SetLastError = true)]
+        public static extern bool InternetSetOption(IntPtr hInternet, InternetOption dwOption, IntPtr lpBuffer, int lpdwBufferLength);
+
+        /// <summary>
+        /// Queries an Internet option on the specified handle. The Handle will be always 0.
+        /// </summary>
+        [DllImport("wininet.dll", CharSet = CharSet.Ansi, SetLastError = true, EntryPoint = "InternetQueryOption")]
+        public extern static bool InternetQueryOptionList(IntPtr Handle, InternetOption OptionFlag, ref InternetPerConnectionOptionList OptionList, ref int size);
+
+
+        public const int INTERNET_OPTION_PROXY = 38;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using SHDocVw;
+using FryProxy;
+using FryProxy.Logging;
+
+namespace IEProxyTest
+{
+    class Program
+    {
+        private static List<string> uris = new List<string>();
+        private static HttpProxyServer server;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Note: The "local" URLs below can be achieved by adding entries to one's
+            // hosts file (C:\Windows\System32\drivers\etc\hosts), similar to the following:
+            //     127.0.0.1   www.seleniumhq-test.test
+            //     127.0.0.1   www.seleniumhq- test-alternate.test
+            // The "remote" URL can be any URL to a server not hosted on localhost.
+            const string LocalBypassedUrl = "http://www.seleniumhq-test-alternate.test:2310/common/simpleTest.html";
+            const string LocalProxiedUrl = "http://www.seleniumhq-test.test:2310/common/simpleTest.html";
+            const string RemoteUrl = "http://webdriver-herald.herokuapp.com";
+
+            // To avoid logging all of the traffic put through the proxy, set to false.
+            bool isLoggingDebugInfoFromProxy = true;
+            string hostName = "127.0.0.1";
+
+            Console.WriteLine("Starting proxy instance");
+            StartProxyServer(hostName, isLoggingDebugInfoFromProxy);
+            Console.WriteLine("Proxy started on {0}:{1}", hostName, server.ProxyEndPoint.Port);
+
+            string proxySetting = string.Format("http={0}:{1}", hostName, server.ProxyEndPoint.Port);
+
+            Console.WriteLine("Getting current proxy settings");
+            var originalProxySettings = GetSystemProxySettings();
+
+            Console.WriteLine("Generating custom proxy settings");
+            var customProxySettings = CreateCustomProxySettings(proxySetting, "www.seleniumhq-test-alternate.test");
+
+            Console.WriteLine("Launching Internet Explorer");
+            InternetExplorer ie = new InternetExplorer();
+            ie.Visible = true;
+
+            Console.WriteLine("Setting proxy settings to custom settings");
+            SetSystemProxySettings(customProxySettings);
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Navigating to bypassed URL (should not be proxied)");
+            ie.Navigate2(LocalBypassedUrl);
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Navigating to local URL with hosts file alias (should be proxied)");
+            ie.Navigate2(LocalProxiedUrl);
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Navigating to non-local URL (should be proxied)");
+            ie.Navigate2(RemoteUrl);
+            Thread.Sleep(5000);
+
+            Console.WriteLine("Restoring proxy settings to orignal settings");
+            SetSystemProxySettings(originalProxySettings);
+
+            Console.WriteLine("Closing Internet Explorer");
+            ie.Quit();
+
+            Console.WriteLine("Shutting down proxy on {0}:{1}", server.ProxyEndPoint.Address.ToString(), server.ProxyEndPoint.Port);
+            StopProxyServer();
+            Console.WriteLine("Proxy instance shut down");
+
+            Console.WriteLine();
+            Console.WriteLine("==================================");
+            Console.WriteLine("Resources requested through proxy:");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine(string.Join("\n", uris.ToArray()));
+            Console.WriteLine("==================================");
+            Console.WriteLine();
+            Console.WriteLine("Press <enter> to quit");
+            Console.ReadLine();
+        }
+
+        private static void StartProxyServer(string hostName, bool logDebugProxyTraffic)
+        {
+            server = new HttpProxyServer(hostName, new HttpProxy());
+            if (logDebugProxyTraffic)
+            {
+                server.Log += OnServerLog;
+            }
+
+            server.Proxy.OnResponseSent = context =>
+            {
+                string[] parts = context.RequestHeader.RequestURI.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length != 0)
+                {
+                    string finalPart = parts[parts.Length - 1];
+                    uris.Add(finalPart);
+                }
+            };
+
+            server.Start().WaitOne();
+        }
+
+        private static void StopProxyServer()
+        {
+            server.Stop();
+        }
+
+        private static void OnServerLog(object sender, LogEventArgs e)
+        {
+            Console.WriteLine(e.LogMessage);
+        }
+
+        private static NativeMethods.InternetPerConnectionOptionList GetSystemProxySettings()
+        {
+            // Query following options.
+            NativeMethods.InternetPerConnectionOption[] options = new NativeMethods.InternetPerConnectionOption[3];
+
+            options[0] = new NativeMethods.InternetPerConnectionOption();
+            options[0].Option = (int)NativeMethods.InternetPerConnectionOptionValue.Flags;
+            options[1] = new NativeMethods.InternetPerConnectionOption();
+            options[1].Option = (int)NativeMethods.InternetPerConnectionOptionValue.ProxyServer;
+            options[2] = new NativeMethods.InternetPerConnectionOption();
+            options[2].Option = (int)NativeMethods.InternetPerConnectionOptionValue.ProxyBypass;
+
+            // Allocate a block of memory of the options.
+            IntPtr buffer = Marshal.AllocCoTaskMem(Marshal.SizeOf(options[0]) + Marshal.SizeOf(options[1]) + Marshal.SizeOf(options[2]));
+
+            IntPtr current = (IntPtr)buffer;
+
+            // Marshal data from a managed object to an unmanaged block of memory.
+            for (int i = 0; i < options.Length; i++)
+            {
+                Marshal.StructureToPtr(options[i], current, false);
+                current = (IntPtr)((int)current + Marshal.SizeOf(options[i]));
+            }
+
+            // Initialize a INTERNET_PER_CONN_OPTION_LIST instance.
+            NativeMethods.InternetPerConnectionOptionList proxySettings = new NativeMethods.InternetPerConnectionOptionList();
+
+            // Point to the allocated memory.
+            proxySettings.OptionsBufferPointer = buffer;
+
+            proxySettings.Size = Marshal.SizeOf(proxySettings);
+
+            // IntPtr.Zero means LAN connection.
+            proxySettings.Connection = IntPtr.Zero;
+
+            proxySettings.OptionCount = options.Length;
+            proxySettings.OptionError = 0;
+            int size = Marshal.SizeOf(proxySettings);
+
+            // Query internet options.
+            bool result = NativeMethods.InternetQueryOptionList(IntPtr.Zero, NativeMethods.InternetOption.PerConnectionOption, ref proxySettings, ref size);
+            if (!result)
+            {
+                throw new ApplicationException(" Set Internet Option Failed! ");
+            }
+
+            return proxySettings;
+        }
+
+        private static NativeMethods.InternetPerConnectionOptionList CreateCustomProxySettings(string proxyString, string proxyBypassString)
+        {
+            // Create 3 options.
+            NativeMethods.InternetPerConnectionOption[] options = new NativeMethods.InternetPerConnectionOption[3];
+
+            // Set PROXY flags.
+            options[0] = new NativeMethods.InternetPerConnectionOption();
+            options[0].Option = (int)NativeMethods.InternetPerConnectionOptionValue.Flags;
+            options[0].Value.integerValue = (int)NativeMethods.InternetPerConnectionProxyFlags.Proxy;
+
+            // Set proxy name.
+            options[1] = new NativeMethods.InternetPerConnectionOption();
+            options[1].Option = (int)NativeMethods.InternetPerConnectionOptionValue.ProxyServer;
+            options[1].Value.stringValue = Marshal.StringToHGlobalAnsi(proxyString);
+
+            // Set proxy bypass.
+            options[2] = new NativeMethods.InternetPerConnectionOption();
+            options[2].Option = (int)NativeMethods.InternetPerConnectionOptionValue.ProxyBypass;
+            options[2].Value.stringValue = Marshal.StringToHGlobalAnsi(proxyBypassString);
+
+            // Allocate a block of memory of the options.
+            IntPtr buffer = Marshal.AllocCoTaskMem(Marshal.SizeOf(options[0]) + Marshal.SizeOf(options[1]) + Marshal.SizeOf(options[2]));
+
+            IntPtr current = buffer;
+
+            // Marshal data from a managed object to an unmanaged block of memory.
+            for (int i = 0; i < options.Length; i++)
+            {
+                Marshal.StructureToPtr(options[i], current, false);
+                current = (IntPtr)((int)current + Marshal.SizeOf(options[i]));
+            }
+
+            // Initialize a INTERNET_PER_CONN_OPTION_LIST instance.
+            NativeMethods.InternetPerConnectionOptionList proxySettings = new NativeMethods.InternetPerConnectionOptionList();
+
+            // Point to the allocated memory.
+            proxySettings.OptionsBufferPointer = buffer;
+
+            // Return the unmanaged size of an object in bytes.
+            proxySettings.Size = Marshal.SizeOf(proxySettings);
+
+            // IntPtr.Zero means LAN connection.
+            proxySettings.Connection = IntPtr.Zero;
+
+            proxySettings.OptionCount = options.Length;
+            proxySettings.OptionError = 0;
+            return proxySettings;
+        }
+
+        private static bool SetSystemProxySettings(NativeMethods.InternetPerConnectionOptionList proxySettings)
+        {
+            int size = Marshal.SizeOf(proxySettings);
+
+            // Allocate memory.
+            IntPtr proxySettingBufferPointer = Marshal.AllocCoTaskMem(size);
+
+            // Convert structure to IntPtr
+            Marshal.StructureToPtr(proxySettings, proxySettingBufferPointer, true);
+
+            // Set internet options.
+            bool bReturn = NativeMethods.InternetSetOption(IntPtr.Zero, NativeMethods.InternetOption.PerConnectionOption, proxySettingBufferPointer, size);
+
+            // Free the allocated memory.
+            Marshal.FreeCoTaskMem(proxySettings.OptionsBufferPointer);
+            Marshal.FreeCoTaskMem(proxySettingBufferPointer);
+
+            if (!bReturn)
+            {
+                throw new ApplicationException(" Set Internet Option Failed! ");
+            }
+
+            // Notify the system that the registry settings have been changed and cause
+            // the proxy data to be reread from the registry for a handle.
+            NativeMethods.InternetSetOption(IntPtr.Zero, NativeMethods.InternetOption.SettingsChanged, IntPtr.Zero, 0);
+            NativeMethods.InternetSetOption(IntPtr.Zero, NativeMethods.InternetOption.Refresh, IntPtr.Zero, 0);
+
+            return bReturn;
+        }
+    }
+}
